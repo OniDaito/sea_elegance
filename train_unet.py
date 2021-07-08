bebc0372511148d326fe46a430f432807eaef935
Benjamin Blundell <me@benjamin.computer>
2021-07-08 16:55:21 +0100

    Changed up the u-net and added a sigmoid to the output image

--- train_unet.py
+++ train_unet.py
@@ -57,8 +57,8 @@ def test(args, model, test_data: DataLoader, step: int, writer: SummaryWriter):
     # Pass output through a sigmnoid for single class prediction
     sigged = torch.sigmoid(result)
     gated = torch.gt(sigged, 0.5)
-    print(gated.int())
-    predict_grid = torchvision.utils.make_grid(torch.int(gated), normalize=True)
+    final = gated.int()
+    predict_grid = torchvision.utils.make_grid(final, normalize=True)
     target_grid = torchvision.utils.make_grid(target_asi)
 
     # show images
