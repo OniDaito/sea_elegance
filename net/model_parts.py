c1a8e38f920605852230be624d0eb4478088b5e0
Benjamin Blundell <me@benjamin.computer>
2021-07-29 11:13:42 +0100

    Scaling all images by 0.5

--- net/model_parts.py
+++ net/model_parts.py
@@ -66,8 +66,8 @@ class Up(nn.Module):
     def forward(self, x1, x2):
         x1 = self.up(x1)
         # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
+        diffY = x2.size()[3] - x1.size()[3]
+        diffX = x2.size()[4] - x1.size()[4]
 
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
                         diffY // 2, diffY - diffY // 2])
