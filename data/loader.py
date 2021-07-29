aa930c8abc991770658129e9ae899e58ba34d63a
Benjamin Blundell <me@benjamin.computer>
2021-07-29 11:44:35 +0100

    Scaling all images by 0.5

--- data/loader.py
+++ data/loader.py
@@ -48,27 +48,27 @@ class WormDataset(Dataset):
         with fits.open(img_path) as w:
             hdul = w[0].data.byteswap().newbyteorder()
             source_image = np.array(hdul).astype("int16")
-            source_image = nd.interpolation.zoom(source_image, zoom=0.25)
+            source_image = nd.interpolation.zoom(source_image, zoom=0.5)
             source_image = np.expand_dims(source_image, axis=0)
-            source_image = torch.tensor(source_image, dtype=torch.float32, device = self.device)
+            source_image = torch.tensor(source_image, dtype=torch.float16, device = self.device)
 
         img_path = os.path.join(self.img_dir, self.img_targets.iloc[idx, 1])
 
         with fits.open(img_path) as w:
             hdul = w[0].data.byteswap().newbyteorder()
             target_asi = np.array(hdul).astype("int8")
-            target_asi = nd.interpolation.zoom(target_asi, zoom=0.25)
+            target_asi = nd.interpolation.zoom(target_asi, zoom=0.5)
             target_asi = np.expand_dims(target_asi, axis=0)
-            target_asi = torch.tensor(target_asi, dtype=torch.float32, device = self.device)
+            target_asi = torch.tensor(target_asi, dtype=torch.float16, device = self.device)
    
         img_path = os.path.join(self.img_dir, self.img_targets.iloc[idx, 2])
         
         with fits.open(img_path) as w:
             hdul = w[0].data.byteswap().newbyteorder()
             target_asj = np.array(hdul).astype("int8")
-            target_asj = nd.interpolation.zoom(target_asj, zoom=0.25)
+            target_asj = nd.interpolation.zoom(target_asj, zoom=0.5)
             target_asj = np.expand_dims(target_asj, axis=0)
-            target_asj = torch.tensor(target_asj, dtype=torch.float32, device = self.device)
+            target_asj = torch.tensor(target_asj, dtype=torch.float16, device = self.device)
 
         if self.transform:
             source_image = self.transform(source_image)
