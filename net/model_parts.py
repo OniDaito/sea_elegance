35a7cf6501c5a6550ee4cf259cfe6604b067991c
Benjamin Blundell <me@benjamin.computer>
2021-07-29 11:38:52 +0100

    Scaling all images by 0.5

--- net/model_parts.py
+++ net/model_parts.py
@@ -70,8 +70,6 @@ class Up(nn.Module):
         diffY = x2.size()[3] - x1.size()[3]
         diffX = x2.size()[4] - x1.size()[4]
 
-        print("shapes", x1.shape, x2.shape)
-
         x1 = F.pad(x1, [
                         diffX // 2, diffX - diffX // 2,
                         diffY // 2, diffY - diffY // 2,
@@ -79,7 +77,6 @@ class Up(nn.Module):
         # if you have padding issues, see
         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-        print("shapes2", x1.shape, x2.shape)
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
