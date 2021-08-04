90c0eed6f22d3b5f2440baa51588d89cbe30e0e4
Benjamin Blundell <me@benjamin.computer>
2021-08-04 17:06:09 +0100

    Trying rprop optimiser

--- train_unet.py
+++ train_unet.py
@@ -171,7 +171,7 @@ if __name__ == "__main__":
     # Adam optimiser results in NaNs which is a real shame
     #optimiser = optim.Adam(variables, lr=args.lr)
     #optimiser = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
-    optimiser = optim.rprop(model.parameters(), lr=args.lr)
+    optimiser = optim.Rprop(model.parameters(), lr=args.lr)
 
 
     # Start Tensorboard
