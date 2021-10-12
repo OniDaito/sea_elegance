35d4c9aa41f0511a959bb20ec1697383deeaf835
Benjamin Blundell <me@benjamin.computer>
2021-10-12 12:05:11 +0100

    Saving training sets to disk?

--- train_unet.py
+++ train_unet.py
@@ -202,7 +202,7 @@ def train(args, model, train_data: DataLoader, test_data: DataLoader,  valid_dat
 
 def dataset_to_disk(args, dataset, filename="dataset.csv"):
     with open(args.savedir + "/" + filename, "w") as f:
-        for (source, target) in dataset:
+        for (source, target) in dataset.img_targets.iloc:
             f.write(source + ", " + target + "\n")
 
 
